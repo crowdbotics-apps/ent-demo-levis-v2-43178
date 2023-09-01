9d5ccda6c0a67226eadc3ae552b1a32422963cb3
Jason Kozel <jason.kozel@crowdbotics.com>
2023-08-31 20:14:50 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import { democonnector_get_productfeedjson_read } from "../../store/demoConnector/democonnector_response_get_productfeeds.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -15,7 +16,7 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.xEhbzBzU}></View></ScrollView>
+    }}><View style={styles.xEhbzBzU}><Text style={styles.YliNzogN}>{"{democonnector_response_get_productfeed[0]} "}</Text></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -29,6 +30,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#E4E4E4",
     borderRadius: 0,
     color: "#777777"
+  },
+  YliNzogN: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled2;
