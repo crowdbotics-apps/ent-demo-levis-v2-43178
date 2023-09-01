f520ff64597fd4245d2328bc35258e1caff218e4
Crowdbotics <engineering@crowdbotics.com>
2023-09-01 00:40:41 +0000

    Update store.

--- store/demoConnector/api.js
+++ store/demoConnector/api.js
@@ -3,4 +3,7 @@ const demoConnector = axios.create({
   baseURL: "https://cb-kozel.github.io/Data",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-export const apiService = {}
+function democonnector_get_productfeedjson_read(payload) {
+  return demoConnector.get(`/product-feed.json`)
+}
+export const apiService = { democonnector_get_productfeedjson_read }
