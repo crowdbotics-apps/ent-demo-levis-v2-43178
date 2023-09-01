e54e482e3041d2e1238210a8c5ebc70109d80e12
Jason Kozel <jason.kozel@crowdbotics.com>
2023-08-31 19:44:26 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/demoConnector/api.js
+++ store/demoConnector/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const demoConnector = axios.create({
   baseURL: "https://cb-kozel.github.io/Data",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function democonnector_get_productfeedjson_read(payload) {
-  return demoConnector.get(`/product-feed.json`)
+  return demoConnector.get(`/product-feed.json`);
 }
-export const apiService = { democonnector_get_productfeedjson_read }
+
+export const apiService = {
+  democonnector_get_productfeedjson_read
+};
