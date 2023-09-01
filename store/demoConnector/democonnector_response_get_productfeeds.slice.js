b70cb12942923d500dff3463bdfca20d019bb2be
Crowdbotics <engineering@crowdbotics.com>
2023-09-01 03:35:43 +0000

    Update store.

--- store/demoConnector/democonnector_response_get_productfeeds.slice.js
+++ store/demoConnector/democonnector_response_get_productfeeds.slice.js
@@ -1,16 +1,15 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const democonnector_get_productfeedjson_read = createAsyncThunk("democonnector_response_get_productfeeds/democonnector_get_productfeedjson_read", async payload => {
-  const response = await apiService.democonnector_get_productfeedjson_read(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const democonnector_get_productfeedjson_read = createAsyncThunk(
+  "democonnector_response_get_productfeeds/democonnector_get_productfeedjson_read",
+  async payload => {
+    const response = await apiService.democonnector_get_productfeedjson_read(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const democonnector_response_get_productfeedsSlice = createSlice({
   name: "democonnector_response_get_productfeeds",
   initialState,
@@ -18,24 +17,27 @@ const democonnector_response_get_productfeedsSlice = createSlice({
   extraReducers: {
     [democonnector_get_productfeedjson_read.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
     [democonnector_get_productfeedjson_read.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
-        state.api.loading = "idle";
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
       }
     },
     [democonnector_get_productfeedjson_read.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   democonnector_get_productfeedjson_read,
   slice: democonnector_response_get_productfeedsSlice
-};
+}
